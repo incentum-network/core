f7c99f8dbdcccc8499e718985de722fce1dd9aca
Bill Donahue <billd@incentum.network>
2019-08-03 13:10:58 +1200

    setup testnet

--- packages/core/bin/config/testnet/plugins.js
+++ packages/core/bin/config/testnet/plugins.js
@@ -4,15 +4,21 @@ module.exports = {
     "@arkecosystem/core-state": {},
     "@incentum/praxis-transactions": {
         enabled: true,
+        network: 23,
         ethStartingBlock: process.env.PRAXIS_ETH_ADDRESS || 7968100,
         ethAddress: process.env.PRAXIS_ETH_ADDRESS,
         ethEndpoint: process.env.PRAXIS_ETH_ENDPOINT,
+        arkGenesis: process.env.PRAXIS_ARK_GENESIS,
+        arkWallet: process.env.PRAXIS_ARK_WALLET,
         arkAddress: process.env.PRAXIS_ARK_ADDRESS,
         arkEndpoint: process.env.PRAXIS_ARK_ENDPOINT,
         authorizedCoinSenderPassphrase: process.env.PRAXIS_COIN_PASSPHRASE,
         itum: process.env.PRAXIS_ITUM_PRICE,
         ethDiscount: process.env.PRAXIS_ETH_DISCOUNT,
         arkDiscount: process.env.PRAXIS_ARK_DISCOUNT,
+        minPurchaseAmount: process.env.PRAXIS_MIN_PURCHASE,
+        maxPurchaseAmount: process.env.PRAXIS_MAX_PURCHASE,
+        authorizedCoinSenderPublicKey: process.env.PRAXIS_SENDER_PUBLICKEY,
     },
     "@arkecosystem/core-database-postgres": {
         connection: {
